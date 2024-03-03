803c36aaea689937475de8b49c5c21b6cd98bb32
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2024-03-03 13:02:33 +0600

    spring batch data csv to db

---
+++ src/main/java/com/example/springbatch/configuration/BatchConfig.java
@@ -0,0 +1,112 @@
+package com.example.springbatch.configuration;
+
+
+import com.example.springbatch.student.Student;
+import com.example.springbatch.student.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class BatchConfig {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+    private final StudentRepository repository;
+
+    @Bean
+    public FlatFileItemReader<Student> reader() {
+        FlatFileItemReader<Student> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/students.csv"));
+        itemReader.setName("csvReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+        return itemReader;
+    }
+
+    @Bean
+    public StudentProcessor processor() {
+        return new StudentProcessor();
+    }
+
+
+    @Bean
+    public RepositoryItemWriter<Student> writer() {
+        RepositoryItemWriter<Student> writer = new RepositoryItemWriter<>();
+        writer.setRepository(repository);
+        writer.setMethodName("save");
+        return writer;
+    }
+
+    @Bean
+    public Step step1() {
+        return new StepBuilder("csvImport", jobRepository)
+                .<Student, Student>chunk(1000, platformTransactionManager)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .taskExecutor(taskExecutor())
+                .build();
+    }
+
+    @Bean
+    public Job runJob() {
+        return new JobBuilder("importStudents", jobRepository)
+                .start(step1())
+                .build();
+
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        SimpleAsyncTaskExecutor asyncTaskExecutor = new SimpleAsyncTaskExecutor();
+        asyncTaskExecutor.setConcurrencyLimit(10);
+        return asyncTaskExecutor;
+    }
+
+    private LineMapper<Student> lineMapper() {
+        DefaultLineMapper<Student> lineMapper = new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("id", "firstName", "lastName", "age");
+
+        BeanWrapperFieldSetMapper<Student> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Student.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+    }
+
+
+//    @Bean
+//    public Tasklet myTasklet() {
+//        return new MyTasklet();
+//    }
+//
+//    @Bean
+//    public Step myStep(JobRepository jobRepository, Tasklet myTasklet, PlatformTransactionManager transactionManager) {
+//        return new StepBuilder("myStep", jobRepository)
+//                .tasklet(myTasklet, transactionManager) // or .chunk(chunkSize, transactionManager)
+//                .build();
+//    }
+
+}
